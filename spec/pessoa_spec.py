457dc504108b53a79f899e72ea4a6eac0faa6358
Rogerio Atem Carvalho <ratem@iff.edu.br>
2014-12-02 20:43:50 -0200

    teste corrigido com inicializacao de atributos de classe

--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -8,6 +8,8 @@ class PessoaSpec(unittest.TestCase):
 		p = Pessoa('t')
 
 	def test_criar_pessoa(self):
+		Pessoa.pessoas = {}
+		Pessoa.contador_id = 0
 		p = Pessoa('joao')
 		p.nome |should| equal_to('joao')
 		p.id |should| equal_to(1)
