fc609586f76d91ece2fb03c29dab2df0ead41397
Rogerio Atem Carvalho <ratem@iff.edu.br>
2014-12-02 21:38:32 -0200

    refatorando e dividindo os testes do construtor

--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -5,31 +5,23 @@ from pessoa import Pessoa
 
 class PessoaSpec(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		pass
-
 	def setUp(self):
 		Pessoa.pessoas = {}
 		Pessoa.contador_id = 0
 		self.p = Pessoa('joao', 'rua dos bobos','enrolado')
 
-	def tearDown(self):
-		pass
+	#Construtor
+	def test_atribui_id(self):
+		self.p.id |should| equal_to(1)
 
-	def test_criar_pessoa(self):
-		#Na pratica anula os efeitos do setUp(), por ser um teste de "inicio do mundo"
-		Pessoa.pessoas = {}
-		Pessoa.contador_id = 0
-		uma_pessoa = Pessoa('joao', 'rua dos bobos','enrolado')
-		#
-		uma_pessoa.nome |should| equal_to('joao')
-		uma_pessoa.endereco |should| equal_to('rua dos bobos')
-		uma_pessoa.estado_civil |should| equal_to('enrolado')
-		uma_pessoa.id |should| equal_to(1)
-		Pessoa.pessoas[1] |should| equal_to(uma_pessoa)
-		#alternativa aa expectativa anterior
-		Pessoa.pessoas |should| include_values(uma_pessoa)
+	def test_armazena_instancia(self):
+		Pessoa.pessoas |should| include_values(self.p)
+
+	def test_inicializa_atributos(self):
+		self.p.nome |should| equal_to('joao')
+		self.p.endereco |should| equal_to('rua dos bobos')
+		self.p.estado_civil |should| equal_to('enrolado')
+	#/Construtor
 
 	def test_alterar_endereco(self):
 		self.p.alterar_endereco('rua das coves')
