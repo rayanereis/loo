98fffba825f3607e30152277f15d68611940d1bc
Rogerio Atem Carvalho <ratem@iff.edu.br>
2014-12-02 21:33:31 -0200

    solucao isolando teste de criacao, mas ficou feio...

--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -4,31 +4,39 @@ from pessoa import Pessoa
 
 
 class PessoaSpec(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
 	def setUp(self):
 		Pessoa.pessoas = {}
 		Pessoa.contador_id = 0
+		self.p = Pessoa('joao', 'rua dos bobos','enrolado')
 
 	def tearDown(self):
 		pass
 
 	def test_criar_pessoa(self):
-		p = Pessoa('joao', 'rua dos bobos','enrolado')
-		p.nome |should| equal_to('joao')
-		p.endereco |should| equal_to('rua dos bobos')
-		p.estado_civil |should| equal_to('enrolado')
-		p.id |should| equal_to(1)
-		Pessoa.pessoas[1] |should| equal_to(p)
+		#Na pratica anula os efeitos do setUp(), por ser um teste de "inicio do mundo"
+		Pessoa.pessoas = {}
+		Pessoa.contador_id = 0
+		uma_pessoa = Pessoa('joao', 'rua dos bobos','enrolado')
+		#
+		uma_pessoa.nome |should| equal_to('joao')
+		uma_pessoa.endereco |should| equal_to('rua dos bobos')
+		uma_pessoa.estado_civil |should| equal_to('enrolado')
+		uma_pessoa.id |should| equal_to(1)
+		Pessoa.pessoas[1] |should| equal_to(uma_pessoa)
 		#alternativa aa expectativa anterior
-		Pessoa.pessoas |should| include_values(p)
+		Pessoa.pessoas |should| include_values(uma_pessoa)
 
 	def test_alterar_endereco(self):
-		p = Pessoa('joao', 'rua dos bobos','enrolado')
-		p.alterar_endereco('rua das coves')
-		p.endereco |should| equal_to('rua das coves')
+		self.p.alterar_endereco('rua das coves')
+		self.p.endereco |should| equal_to('rua das coves')
 
 	def test_alterar_estado_civil(self):
-		p = Pessoa('joao', 'rua dos bobos','enrolado')
-		p.alterar_estado_civil('casado')
-		p.estado_civil |should| equal_to('casado')
+		self.p.alterar_estado_civil('casado')
+		self.p.estado_civil |should| equal_to('casado')
 
 
