5cbeefd82047d414a66d139229cd1bb64fe5264e
Rogerio Atem Carvalho <ratem@iff.edu.br>
2014-12-02 21:19:01 -0200

    Gerenciando estado civil

--- pessoa.py
+++ pessoa.py
@@ -12,11 +12,15 @@ class Pessoa:
 		cls.contador_id = cls.contador_id + 1
 		return cls.contador_id
 
-	def __init__(self, nome, endereco):
+	def __init__(self, nome, endereco, estado_civil):
 		self.nome = nome
 		self.endereco = endereco
+		self.estado_civil = estado_civil
 		self.id = Pessoa.identifica()
 		Pessoa.insere(self)
 
 	def alterar_endereco(self, novo_endereco):
 		self.endereco = novo_endereco
+
+	def alterar_estado_civil(self, novo_estado_civil):
+		self.estado_civil = novo_estado_civil
--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -12,15 +12,23 @@ class PessoaSpec(unittest.TestCase):
 		pass
 
 	def test_criar_pessoa(self):
-		p = Pessoa('joao', 'rua dos bobos')
+		p = Pessoa('joao', 'rua dos bobos','enrolado')
 		p.nome |should| equal_to('joao')
 		p.endereco |should| equal_to('rua dos bobos')
+		p.estado_civil |should| equal_to('enrolado')
 		p.id |should| equal_to(1)
 		Pessoa.pessoas[1] |should| equal_to(p)
 		#alternativa aa expectativa anterior
 		Pessoa.pessoas |should| include_values(p)
 
 	def test_alterar_endereco(self):
-		p = Pessoa('joao', 'rua dos bobos')
+		p = Pessoa('joao', 'rua dos bobos','enrolado')
 		p.alterar_endereco('rua das coves')
 		p.endereco |should| equal_to('rua das coves')
+
+	def test_alterar_estado_civil(self):
+		p = Pessoa('joao', 'rua dos bobos','enrolado')
+		p.alterar_estado_civil('casado')
+		p.estado_civil |should| equal_to('casado')
+
+
