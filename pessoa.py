e7339f12c454b8906c7745b0fa2dc68145854ec5
Rogerio Atem Carvalho <ratem@iff.edu.br>
2014-12-02 20:50:07 -0200

    testando alterar endereco

--- pessoa.py
+++ pessoa.py
@@ -12,7 +12,11 @@ class Pessoa:
 		cls.contador_id = cls.contador_id + 1
 		return cls.contador_id
 
-	def __init__(self, nome):
+	def __init__(self, nome, endereco):
 		self.nome = nome
+		self.endereco = endereco
 		self.id = Pessoa.identifica()
 		Pessoa.insere(self)
+
+	def alterar_endereco(self, novo_endereco):
+		self.endereco = novo_endereco
--- spec/pessoa_spec.py
+++ spec/pessoa_spec.py
@@ -4,14 +4,16 @@ from pessoa import Pessoa
 
 
 class PessoaSpec(unittest.TestCase):
-	def test_aaa(self):
-		p = Pessoa('t')
-
 	def test_criar_pessoa(self):
 		Pessoa.pessoas = {}
 		Pessoa.contador_id = 0
-		p = Pessoa('joao')
+		p = Pessoa('joao', 'rua dos bobos')
 		p.nome |should| equal_to('joao')
+		p.endereco |should| equal_to('rua dos bobos')
 		p.id |should| equal_to(1)
 		Pessoa.pessoas[1] |should| equal_to(p)
 
+	def test_alterar_endereco(self):
+		p = Pessoa('joao', 'rua dos bobos')
+		p.alterar_endereco('rua das coves')
+		p.endereco |should| equal_to('rua das coves')
